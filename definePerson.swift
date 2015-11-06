379ad63cae2b20254e93716753b0ff5a953aada6
kkqin <xolaoqin@gmail.com>
2015-11-06 14:42:07 +0800

    add a file Person

---
+++ definePerson.swift
@@ -0,0 +1,33 @@
+ass Person{
+    var name : String!
+    var age : String!
+    var phoneNum : String = ""{
+        willSet(num){
+            print("phone number will set be \(num)")
+        }
+        didSet{
+            print("phone number is \(oldValue) ")
+        }
+    }
+    
+    var s = "notSetSex"
+    
+    var sex : String {
+        get{ return s }
+        set{ s = newValue}
+    }
+    
+    func sayhello(){
+        print("hello \(self.name)")
+    }
+    
+}
+
+var k = Person()
+
+k.name = "jack"
+k.age = "12"
+k.phoneNum = "1233435"
+k.sex = "female"
+k.sayhello()
+
