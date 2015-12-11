f59ff7dd64345349eceeda965a5a37069032b0de
kkqin <xolaoqin@gmail.com>
2015-12-11 22:46:14 +0800

    添加了导航页

--- HelloUI-2/pageViewController.swift
+++ HelloUI-2/pageViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class pageViewController: UIViewController {
+class pageViewController: UIViewController, UIScrollViewDelegate {
 
     let numOfPages = 3
-    let pageWidth = 414
-    let pageHeight = 736
+//    let pageWidth = 414
+//    let pageHeight = 736
+    
+    var pageControl:UIPageControl!//页控件
     
     override func viewDidLoad(){
      super.viewDidLoad()
@@ -20,19 +22,50 @@ class pageViewController: UIViewController {
         let scrollView = UIScrollView()
         scrollView.frame = self.view.bounds
         
+        
+        let frame = self.view.bounds
         //为了能让内容横向滚动，设置横向内容宽度为3个页面宽度总和
-        scrollView.contentSize = CGSizeMake(CGFloat(pageWidth*numOfPages), CGFloat(pageHeight))
+       scrollView.contentSize=CGSizeMake(frame.size.width*CGFloat(numOfPages),frame.size.height)
         scrollView.pagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.showsVerticalScrollIndicator = false
         scrollView.scrollsToTop = false
+        scrollView.delegate = self
         for i in 0..<numOfPages{
             let myViewController = MyViewController(number: (i+1))
             //设置每一页内容的位置
-            myViewController.view.frame = CGRectMake(CGFloat(pageWidth * i), CGFloat(0),CGFloat(pageWidth),CGFloat(pageHeight))
+            myViewController.view.frame = CGRectMake(frame.size.width*CGFloat(i),CGFloat(0),frame.size.width,frame.size.height)
             scrollView.addSubview(myViewController.view)
+
         }
+        
+        
+        scrollView.contentOffset = CGPointZero
+        
+        self.pageControl=UIPageControl(frame: CGRectMake(self.view.frame.size.width/2-100, self.view.frame.size.height-100, 200, 100))
+        self.pageControl.numberOfPages=3
+        self.pageControl.currentPage=0
+
+        
         self.view.addSubview(scrollView)
+        self.view.addSubview(pageControl)
+    }
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let twidth = CGFloat(numOfPages-1) * self.view.bounds.size.width
+        let offset=scrollView.contentOffset
+        pageControl.currentPage=Int(offset.x/self.view.frame.size.width)
+        
+        if(scrollView.contentOffset.x > twidth)
+        {
+            
+            let SEG = SegViewController()
+            SEG.modalTransitionStyle = UIModalTransitionStyle.CoverVertical//出现方式
+            self.presentViewController(SEG, animated: true, completion:nil)
+        }
+        
+        
     }
     
 }
