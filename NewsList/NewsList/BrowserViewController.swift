68e3ea5dd3aa34921aaed47ad10fcecb015e9ca1
kkqin <asdfa@g>
2021-09-05 23:24:32 +0800

    swift3 -> swift 5

--- NewsList/NewsList/BrowserViewController.swift
+++ NewsList/NewsList/BrowserViewController.swift
@@ -21,21 +21,21 @@ class BrowserViewController: UIViewController,UIWebViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         //1.配置webView
         webView = UIWebView()
-        webView.frame = CGRectMake(0, 64, self.view.bounds.size.width, self.view.bounds.size.height-44)
+        webView.frame = CGRect(x: 0, y: 64, width: self.view.bounds.size.width, height: self.view.bounds.size.height-44)
         webView.scalesPageToFit=true  //自适应
         self.view.addSubview(webView)
         webView.delegate=self
         
         //2.工具条UIToolBar
-        let myToolBar = UIToolbar(frame: CGRectMake(0, self.view.bounds.size.height-44, self.view.bounds.size.width, 44))
-        myToolBar.backgroundColor = UIColor.blackColor()
+        let myToolBar = UIToolbar(frame: CGRect(x: 0, y: self.view.bounds.size.height-44, width: self.view.bounds.size.width, height: 44))
+        myToolBar.backgroundColor = UIColor.black
         self.view.addSubview(myToolBar)
         
         //控制网页刷新、停止、后退、前进的按钮，加载到工具条中
-        let reloadButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh, target: self, action: Selector("reloadDidPush"))
-        stopButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Stop, target: self, action: Selector("stopDidPush"))
-        backButton = UIBarButtonItem(title: "后退", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("backDidPush"))
-        forwardButton = UIBarButtonItem(title: "前进", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("forwardDidPush"))
+        let reloadButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.refresh, target: self, action: Selector(("reloadDidPush")))
+        stopButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.stop, target: self, action: Selector(("stopDidPush")))
+        backButton = UIBarButtonItem(title: "后退", style: UIBarButtonItem.Style.plain, target: self, action: Selector(("backDidPush")))
+        forwardButton = UIBarButtonItem(title: "前进", style: UIBarButtonItem.Style.plain, target: self, action: Selector(("forwardDidPush")))
         myToolBar.setItems([reloadButton,stopButton,backButton,forwardButton], animated: true)
     }
 
@@ -44,11 +44,11 @@ class BrowserViewController: UIViewController,UIWebViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         //通过NSURLRequest方法来加载并显示URL指定的内容
         //var url = NSURL(string: "http://www.apple.com")
-        let request = NSURLRequest(URL: url!)
-        webView.loadRequest(request)
+        let request = NSURLRequest(url: url! as URL)
+        webView.loadRequest(request as URLRequest)
     }
     
     func reloadDidPush(){
@@ -68,22 +68,22 @@ class BrowserViewController: UIViewController,UIWebViewDelegate {
     }
     
     //MARK:控件刷新方法，自定义
-    func updateControlEnabled(){
-        stopButton.enabled = webView.loading
-        backButton.enabled = webView.canGoBack
-        forwardButton.enabled = webView.canGoForward
+    func updateControlEnabled() {
+        stopButton.isEnabled = webView.isLoading
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
     }
     
     //MARK:UIWebViewDelegate协议中的方法
-    func webViewDidStartLoad(webView: UIWebView) {
+    func webViewDidStartLoad(_ webView: UIWebView) {
         updateControlEnabled()
     }
     
-    func webViewDidFinishLoad(webView: UIWebView) {
+    func webViewDidFinishLoad(_ webView: UIWebView) {
         updateControlEnabled()
     }
     
-    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+    private func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
         updateControlEnabled()
     }
 
--- NewsList/NewsList/MainTableViewController.swift
+++ NewsList/NewsList/MainTableViewController.swift
@@ -12,16 +12,15 @@ class MainTableViewController: UITableViewController {
 
     let newsString = "http://c.3g.163.com/nc/article/local/5aSn6L%2Be/0-20.html"
     
-    var news = [] //空数组，放所有的新闻列表
+    var news : NSMutableArray = [] //空数组，放所有的新闻列表
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.registerClass(UITableViewCell.self , forCellReuseIdentifier: "reuseIdentifier")
+        self.tableView.register(UITableViewCell.self , forCellReuseIdentifier: "reuseIdentifier")
         
         self.title="大连新闻"
 
         loadDataSource()  //加载新闻数据
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,17 +33,17 @@ class MainTableViewController: UITableViewController {
         //1.封装NSURL
         let url = NSURL(string: newsString)!
         //2.NSURL封装NSRequest
-        let request = NSURLRequest(URL: url)
+        let request = NSURLRequest(url: url as URL)
         //3.异步链接
-        let quene = NSOperationQueue()
-        UIApplication.sharedApplication().networkActivityIndicatorVisible=true
-        NSURLConnection.sendAsynchronousRequest(request,
+        let quene = OperationQueue()
+        UIApplication.shared.isNetworkActivityIndicatorVisible=true
+        NSURLConnection.sendAsynchronousRequest(request as URLRequest,
             queue: quene) { (response, data, error) -> Void in
                 if error != nil {
                     //出错
-                    print(error!.description)
+                    print(error!.localizedDescription)
                     self.refreshControl?.endRefreshing()
-                UIApplication.sharedApplication().networkActivityIndicatorVisible=false
+                    UIApplication.shared.isNetworkActivityIndicatorVisible=false
                 }
                 else{
                     if data == nil {
@@ -53,14 +52,13 @@ class MainTableViewController: UITableViewController {
                     else{
                         print("处理数据\n")
                         var errorRead:NSError?
-                        let json = (try! NSJSONSerialization.JSONObjectWithData(data!,
-                                    options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                        let json = (try! JSONSerialization.jsonObject(with: data!,
+                                                                      options: JSONSerialization.ReadingOptions.mutableContainers)) as! NSDictionary
                         if errorRead != nil{
                             print("json数据不合法，解析失败")
-                        }
-                        else{
-                            let newsDataSource = json["大连"] as! NSArray
-                            var currentNewsDataSource = NSMutableArray() //新闻项数组
+                        } else {
+                            let newsDataSource = json["大连"] as! [[String:Any]]
+                            let currentNewsDataSource = NSMutableArray() //新闻项数组
                             for currentNews in newsDataSource {
                                 let newsItem = NewsItem()
                                 //新闻标题
@@ -71,16 +69,15 @@ class MainTableViewController: UITableViewController {
                                 //currentNews["imgsrc"] as! String //图片链接地址
                                 
                                 //把新闻实例加入到数组中
-                                currentNewsDataSource.addObject(newsItem)
+                                currentNewsDataSource.add(newsItem)
                             }
                             
-                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            DispatchQueue.main.async {
                                 self.news = currentNewsDataSource
                                 self.tableView.reloadData()
                                 self.refreshControl?.endRefreshing()
-                                UIApplication.sharedApplication().networkActivityIndicatorVisible=false
-                            })
-                            
+                                UIApplication.shared.isNetworkActivityIndicatorVisible=false
+                            }
                         }
                     }
                 }
@@ -89,32 +86,31 @@ class MainTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.news.count
     }
-
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath as IndexPath)
 
         // Configure the cell...
         let aNew = self.news[indexPath.row] as! NewsItem
         
         cell.textLabel?.text = aNew.newsTitle
         
-        cell.accessoryType=UITableViewCellAccessoryType.DisclosureIndicator
+        cell.accessoryType=UITableViewCell.AccessoryType.disclosureIndicator
         
         return cell
     }
     
     //点击单元格，跳转到新闻详情界面,UITableViewDelegate协议的方法之一
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let row = indexPath.row
         let data = self.news[row] as! NewsItem
         //入栈
