9dd1fe97d67709c9545e4d05be842edb27d5fbde
kkqin <asdfa@g>
2021-09-06 00:10:51 +0800

    swift3 -> swift 5

--- WeScroll/WeScroll/GuideViewController.swift
+++ WeScroll/WeScroll/GuideViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class GuideViewController: UIViewController, UIScrollViewDelegate {
-
     var numOfPages = 3
     var pageControl:UIPageControl!//页控件
     
@@ -21,10 +20,10 @@ class GuideViewController: UIViewController, UIScrollViewDelegate {
         scrollView.frame=self.view.bounds
         scrollView.delegate = self
         //为了能让内容横向滚动，设置横向内容宽度为3个页面的宽度总和
-        scrollView.contentSize=CGSizeMake(frame.size.width*CGFloat(numOfPages),frame.size.height)
+        scrollView.contentSize=CGSize(width: frame.size.width*CGFloat(numOfPages),height: frame.size.height)
         print("\(frame.size.width*CGFloat(numOfPages)),\(frame.size.height)")
         
-        scrollView.pagingEnabled=true
+        scrollView.isPagingEnabled=true
         scrollView.showsHorizontalScrollIndicator=false
         scrollView.showsVerticalScrollIndicator=false
         scrollView.scrollsToTop=false
@@ -33,14 +32,14 @@ class GuideViewController: UIViewController, UIScrollViewDelegate {
             print(imgfile)
             let image = UIImage(named:"\(imgfile)")
             let imgView = UIImageView(image: image)
-            imgView.frame=CGRectMake(frame.size.width*CGFloat(i),CGFloat(0),
-                frame.size.width,frame.size.height)
+            imgView.frame=CGRect(x: frame.size.width*CGFloat(i),y: CGFloat(0),
+                                 width: frame.size.width,height: frame.size.height)
             scrollView.addSubview(imgView)
         }
-        scrollView.contentOffset = CGPointZero
+        scrollView.contentOffset = CGPoint(x: 0,y: 0)
         
         //pageControl页控件初始化
-        pageControl=UIPageControl(frame: CGRectMake(self.view.frame.size.width/2-100, self.view.frame.size.height-100, 200, 100))
+        pageControl=UIPageControl(frame: CGRect(x: self.view.frame.size.width/2-100, y: self.view.frame.size.height-100, width: 200, height: 100))
         pageControl.numberOfPages=3
         pageControl.currentPage=0
         
@@ -50,8 +49,7 @@ class GuideViewController: UIViewController, UIScrollViewDelegate {
         
     }
     //MARK: scrollView委托事件
-    func scrollViewDidScroll(scrollView: UIScrollView)
-    {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         print("scrolled:\(scrollView.contentOffset)")
         let twidth = CGFloat(numOfPages-1) * self.view.bounds.size.width
         
@@ -62,8 +60,7 @@ class GuideViewController: UIViewController, UIScrollViewDelegate {
             let mainStoryboard = UIStoryboard(name:"Main", bundle:nil)
             let viewController = mainStoryboard.instantiateInitialViewController()! as UIViewController
             
-            self.presentViewController(viewController, animated: true, completion:nil)
+            self.present(viewController, animated: true, completion:nil)
         }
     }
-    
 }
